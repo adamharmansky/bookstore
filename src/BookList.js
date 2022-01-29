f767a525f15f6bf4ddd6c26ef500af413ccbfd50
Plundration <matouinhell@gmail.com>
2022-01-29 16:02:21 +0100

    micv

--- src/BookList.js
+++ src/BookList.js
@@ -43,7 +43,7 @@ export default function BookList() {
 						</a>
 					)
 				})}
-				<p> {bookList.length === 0 ? "No matches Found." : ""} </p>
+				<p> {bookList.length === 0 ? "Neboli nájdené žiadne knihy" : ""} </p>
 			</div>
 		</div>
 	)
