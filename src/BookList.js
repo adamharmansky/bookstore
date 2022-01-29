a07af565f08f3b1470f4c8ba1eb1abd6fff9cb87
Adam Harmansky <micv@firemail.cc>
2022-01-29 12:53:59 +0100

    removed the div, added display=block

--- src/BookList.js
+++ src/BookList.js
@@ -26,7 +26,6 @@ export default function BookList() {
 				{bookList.map((value, key) => {
 					return (
 						<a className='BookListItem' href={thisPageUrl.href + value.isbn}>
-							<div>
 								<img className='BookListItemImage' src={value.image ? value.image : "http://www.harmansky.xyz/vlc.png"} alt='Missing'></img>
 								<div className='BookListItemInfo'>
 									<div className='BookListItemTitle'> {value.title} </div>
@@ -35,7 +34,6 @@ export default function BookList() {
 									<div className="BookListItemYear"> {value.year_pub} </div>
 								</div>
 								<p className='BookListDescription'> {value.description ? value.description : "No Description"} </p>
-							</div>
 						</a>
 					)
 				})}
