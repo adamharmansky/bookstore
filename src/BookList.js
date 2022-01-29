0bd28ea6b935cbcdcbe8bc6a6700dbded965222d
Adam Harmansky <micv@firemail.cc>
2022-01-29 12:49:45 +0100

    Changed Flex to paragraph in booklist

--- src/BookList.js
+++ src/BookList.js
@@ -26,14 +26,14 @@ export default function BookList() {
 				{bookList.map((value, key) => {
 					return (
 						<a className='BookListItem' href={thisPageUrl.href + value.isbn}>
-                            <img className='BookListItemImage' src={value.image ? value.image : "http://www.harmansky.xyz/vlc.png"} alt='Missing'></img>
-                            <div className='BookListItemInfo'>
-	    						<div className='BookListItemTitle'> {value.title} </div>
-    							<div className='BookListItemAuthor'> {value.author_name} </div>
-                                <div className="BookListItemPublish"> {value.publisher_name ? value.publisher_name : "MICV Publishing INC."} </div>
-                                <div className="BookListItemYear"> {value.year_pub} </div>
-                            </div>
-							<div className='BookListDescription'> {value.description ? value.description : "No Description"} </div>
+							<img className='BookListItemImage' src={value.image ? value.image : "http://www.harmansky.xyz/vlc.png"} alt='Missing'></img>
+							<div className='BookListItemInfo'>
+								<div className='BookListItemTitle'> {value.title} </div>
+								<div className='BookListItemAuthor'> {value.author_name} </div>
+								<div className="BookListItemPublish"> {value.publisher_name ? value.publisher_name : "MICV Publishing INC."} </div>
+								<div className="BookListItemYear"> {value.year_pub} </div>
+							</div>
+							<p className='BookListDescription'> {value.description ? value.description : "No Description"} </p>
 						</a>
 					)
 				})}
